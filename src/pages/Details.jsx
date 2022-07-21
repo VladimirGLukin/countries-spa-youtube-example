ca5fad267a02d436853eb670bd4c359a2fd1df6c
DetiToDeti <91261759+DetiToDeti@users.noreply.github.com>
2022-07-21 03:44:11 +0300

    before big changes:)

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { IoArrowBack } from 'react-icons/io5';
 import { searchByCountry } from '../config';
+//import { Button } from '@salutejs/plasma-ui'; //'../components/Button';
 import { Button } from '../components/Button';
 import { Info } from '../components/Info';
 
@@ -18,11 +19,14 @@ export const Details = () => {
   }, [name]);
 
   return (
+    <>
     <div>
-      <Button onClick={goBack}>
-        <IoArrowBack /> Back
+      <Button text="Back" contentLeft={<IoArrowBack />} onClick={goBack}>
+        <IoArrowBack />
+        <span>Back</span>
       </Button>
       {country && <Info push={push} {...country} />}
     </div>
+  </>
   );
 };
